1fd71e55b0aa7fb5547442ea455ac1e149da8a99
shin-SeungHun <sng7387@gmail.com>
2024-03-26 18:13:49 +0900

    Initial Commit

---
+++ ConcurrencyJourney/ConcurrencyJourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyJourneyApp.swift
+//  ConcurrencyJourney
+//
+//  Created by 신승훈 on 2024/03/26.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyJourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
