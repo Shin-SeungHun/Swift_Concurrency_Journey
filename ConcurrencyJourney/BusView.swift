0484df649f9e7d004076a1ec1776c83376c9bc7f
shin-SeungHun <sng7387@gmail.com>
2024-03-26 18:29:27 +0900

    refactor: Result로 보내는 에러

--- ConcurrencyJourney/BusView.swift
+++ ConcurrencyJourney/BusView.swift
@@ -10,11 +10,21 @@ import SwiftUI
 class BusManager {
     let isActive: Bool = false
     
+    /// 튜플
     func changeDisplay() -> (title: String?, error: Error? ) {
         if isActive {
             return ("버스가 출발합니다", nil)
         } else {
             return (nil, URLError(.unknown))
+        }
+    }
+    
+    /// Result
+    func changeDisplay2() -> Result<String, Error> {
+        if isActive {
+            return .success("버스가 출발합니다")
+        } else {
+            return .failure(URLError(.unknown))
         }
     }
 }
@@ -25,10 +35,21 @@ class BusViewModel: ObservableObject {
     let manager = BusManager()
     
     func driving() {
+        /*
         let returnedValue = manager.changeDisplay()
         if let newDisplay = returnedValue.title {
             self.display = newDisplay
         } else if let error = returnedValue.error {
+            self.display = error.localizedDescription
+        }
+        */
+        
+        let result = manager.changeDisplay2()
+        
+        switch result {
+        case .success(let newDisplay):
+            self.display = newDisplay
+        case .failure(let error):
             self.display = error.localizedDescription
         }
     }
